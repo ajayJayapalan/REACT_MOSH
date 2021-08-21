15478aa5c3608f9b73b78b984dce5071ac025359
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-21 07:00:32 +0530

    All about Routing

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Customers from "./components/customers";
+import Rental from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from './components/movieForm';
 
 class App extends Component {
   state = {};
   render() {
     return (
       <>
+        <NavBar />
         <main className="container">
-          <Movies/>
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rental}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
         </main>
       </>
     );
--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Like from "./like";
 import Table from "./../common/table";
+import { Link } from 'react-router-dom';
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    { path: "title", label: "Title", content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
@@ -21,7 +22,7 @@ class MoviesTable extends Component {
           onClick={() => {
             this.props.onDelete(movie._id);
           }}
-          className="btn btn-danger btn-sm m-3"
+          className="btn btn-danger btn-sm "
         >
           DELETE
         </button>
