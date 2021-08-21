1089d2472dfcad9cb22c2c07b3c9bc60bafd6e98
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-21 09:36:33 +0530

    Basic Validation

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
 import Movies from "./components/movies";
 import NavBar from "./components/navbar";
 import Customers from "./components/customers";
 import Rental from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
+import "./App.css";
 
 class App extends Component {
   state = {};
@@ -16,6 +17,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
