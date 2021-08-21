f8186ae1b633b2072b1693828a74b8f3fa8f6be7
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-21 11:18:55 +0530

    LoginForm Refactored

--- vidly/src/common/input.jsx
+++ vidly/src/common/input.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-const Input = ({ name, label, error, value, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
-        autoFocus
-        value={value}
-        onChange={onChange}
+        {...rest}
         name={name}
         id={name}
-        type="text"
         className="form-control"
       />
       {error && <div className="alert alert-danger">{error}</div>}
